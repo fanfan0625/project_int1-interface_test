492316f2f490a5716a193f22aeb006ffa922cc4a
fanfan0625 <570609008@qq.com>
2018-06-30 15:02:59 +0800

    '第二次提交'

--- test_case/test_ranzhi_login.py
+++ test_case/test_ranzhi_login.py
@@ -26,13 +26,23 @@ class testRanzhilogin(unittest.TestCase):
         #1.请求登录页
         res = requests.get(self.url)
         #2.通过正则表达式获取v.random的值
-        pattern = "(?=v.random = \).*(?=;<)"
-
-        #3.对password进行加密
-        #4.构造请求参数
-        #5.发送请求
-        #6.发送结果
-
+		pattern = "(?=v.random = \").*(?=;<)"
+        result = re.search(pattern, res.text).group()
+        random_str = result.replace('v.random = "', '').replace('"', '')
+        #3. 对password进行加密
+        step1 = hashlib.md5(random_str.encode('utf-8')).hexdigest()
+        tempstr = step1 + self.username
+        step2 = hashlib.md5(tempstr.encode('utf-8')).hexdigest()
+        tempstr = step2 + random_str
+        newpassword = hashlib.md5(tempstr.encode('utf-8')).hexdigest()
+        #newpassword = hashlib.md5(hashlib.md5(hashlib.md5(random_str.encode('utf-8')) + self.username.encode('utf-8')) + random_str.encode('utf-8'))
+        rawpassword = hashlib.md5(newpassword.encode('utf-8')).hexdigest()
+        #4. 构造请求参数
+        payload = {"account":self.username,"password":newpassword,"referer":"/","rawPassword":rawpassword,"keepLogin":"false"}
+        #5. 发送请求
+        res = requests.post(self.url, json=payload)
+        #6. 校验结果
+        self.assertIn('<html>', res.text)
         pass
 
 if __name__ == '__main__':
